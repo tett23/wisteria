bbd4361babd35fca51e19841a1cc60bdc15757d7
tett23 <tett23@gmail.com>
2021-05-09 19:22:46 +0900

    Improve performance

--- renderer/components/Preview/Page/index.tsx
+++ renderer/components/Preview/Page/index.tsx
@@ -1,21 +1,82 @@
 import classNames from 'classnames';
+import { useEffect, useRef, useState } from 'react';
 import { usePageContent } from '../modules/pageMetrics';
 import { Paragraph } from '../Paragraph';
 import css from '../Preview.module.css';
+import { BlankPage } from './BlankPage';
 
-export function Page({
+export function Page(props: { text: string; pageNumber: number }) {
+  const { ref, isIntersecting, pageNumber, offset, paragraphs } = usePageProps(
+    props,
+  );
+
+  return (
+    <div ref={ref}>
+      {isIntersecting ? (
+        <PageContent
+          pageNumber={pageNumber}
+          offset={offset}
+          paragraphs={paragraphs}
+        />
+      ) : (
+        <BlankPage pageNumber={pageNumber} />
+      )}
+    </div>
+  );
+}
+
+function usePageProps({
   text,
   pageNumber,
 }: {
-  text: string;
   pageNumber: number;
+  text: string;
 }) {
   const [[startIndex, endIndex], offset] = usePageContent(pageNumber);
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    if (ref.current == null) {
+      return;
+    }
+
+    const observer = new IntersectionObserver((entries) => {
+      const [el] = entries;
+      if (el == null) {
+        return;
+      }
+
+      setIsIntersecting(el.isIntersecting);
+    });
+    observer.observe(ref.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref]);
   const lines = text.split('\n').slice(startIndex, endIndex);
   const paragraphs = lines.map((l, i) => (
     <Paragraph key={i + l} text={l}></Paragraph>
   ));
 
+  return {
+    ref,
+    isIntersecting,
+    pageNumber,
+    offset,
+    paragraphs,
+  };
+}
+
+function PageContent({
+  pageNumber,
+  offset,
+  paragraphs,
+}: {
+  pageNumber: number;
+  offset: number;
+  paragraphs: JSX.Element[];
+}) {
   return (
     <div className={classNames(css.pageOuter, 'select-none')}>
       <PageNumber pageNumber={pageNumber} />
