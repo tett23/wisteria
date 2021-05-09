463f87332a2e611980278ee192c60f07d0bf56e4
tett23 <tett23@gmail.com>
2021-05-09 19:41:36 +0900

    Tweak layout

--- renderer/components/Editor/index.tsx
+++ renderer/components/Editor/index.tsx
@@ -1,17 +1,31 @@
 import { useEditor } from 'modules/editor/useEditor';
+import { useCallback, useRef } from 'react';
 
 export function Editor() {
   const { file, onChange, disabled } = useEditor();
+  const ref = useRef<HTMLTextAreaElement>(null);
+  const onClick = useCallback(() => {
+    if (ref.current == null) {
+      return;
+    }
+
+    ref.current.focus();
+  }, [ref.current]);
   const elem = [
     <textarea
       key={file?.path ?? 'none'}
-      className="border-2 outline-none w-full h-full"
-      style={{ overflowAnchor: 'none' }}
+      className="outline-none h-full mx-4"
+      style={{ width: '42rem', overflowAnchor: 'none' }}
       onChange={onChange}
       disabled={disabled}
       defaultValue={file?.body}
+      ref={ref}
     ></textarea>,
   ];
 
-  return <>{elem}</>;
+  return (
+    <div onClick={onClick} className="w-full h-full text-center py-4">
+      {elem}
+    </div>
+  );
 }
