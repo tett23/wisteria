0ec270ec453a681cb5ac07b58010519b0d0007d2
tett23 <tett23@gmail.com>
2021-05-09 18:39:22 +0900

    Tweak layout

--- renderer/components/MainView.tsx
+++ renderer/components/MainView.tsx
@@ -11,35 +11,35 @@ export type MainViewProps = {};
 export function MainView(_: MainViewProps) {
   return (
     <div className="w-screen h-screen overflow-hidden">
-      <div className="flex w-screen h-screen">
-        <Resizable
-          direction="vertical"
-          className="w-full h-full flex"
-          defaultSize="30vw"
-        >
-          <div className="h-screen flex">
-            <div className="w-full border-r-2 border-gray-500">
-              <ProjectViewContainer />
-            </div>
-            <div>
-              <FileView></FileView>
-            </div>
+      <Resizable
+        direction="vertical"
+        className="w-screen h-screen flex"
+        defaultSize="30vw"
+      >
+        <div className="h-full flex">
+          <div className="w-full border-r-2 border-gray-500">
+            <ProjectViewContainer />
           </div>
-          <div className="h-screen w-full">
-            <BufferInfoWC></BufferInfoWC>
-            <Resizable
-              direction="horizontal"
-              className="w-full h-full"
-              defaultSize="80vh"
-            >
+          <div>
+            <FileView />
+          </div>
+        </div>
+        <div className="h-full w-full">
+          <Resizable
+            direction="horizontal"
+            className="w-full h-full"
+            defaultSize="80vh"
+          >
+            <div className="w-full h-full overflow-hidden">
+              <BufferInfoWC />
               <Preview />
-              <div className="w-full h-full">
-                <Editor></Editor>
-              </div>
-            </Resizable>
-          </div>
-        </Resizable>
-      </div>
+            </div>
+            <div className="w-full h-full">
+              <Editor />
+            </div>
+          </Resizable>
+        </div>
+      </Resizable>
       <CalculatePageMetricsContainer />
     </div>
   );
