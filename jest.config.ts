f3ab98574205ede94578d16286e270215ca77ef1
tett23 <tett23@gmail.com>
2021-06-07 23:58:32 +0900

    Add resolve path to renderer/asyncActions

--- jest.config.ts
+++ jest.config.ts
@@ -9,6 +9,7 @@ export default {
     'modules(.*)': '<rootDir>/renderer/modules$1',
     'models(.*)': '<rootDir>/models$1',
     'hooks(.*)': '<rootDir>/renderer/hooks$1',
+    'asyncActions(.*)': '<rootDir>/renderer/asyncActions$1',
     'assets(.*)': '<rootDir>/renderer/assets$1',
     'workers(.*)': '<rootDir>/workers$1',
   },
