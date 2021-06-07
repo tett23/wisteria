38ccbd7271e2a0feefe4190588c2392dc9b3d4b7
tett23 <tett23@gmail.com>
2021-06-07 23:59:16 +0900

    Move calcurateMetrics logic

--- renderer/modules/preview/useCalcPageMetrics.ts
+++ renderer/modules/preview/useCalcPageMetrics.ts
@@ -1,7 +1,7 @@
 import { useRecoilValue, useSetRecoilState } from 'recoil';
-import { useCallback, useEffect } from 'react';
-import PromiseWorker from 'promise-worker';
+import { useEffect } from 'react';
 import { previewBlocks, previewPageWidth, previewPages } from './index';
+import { calcuratePageMetrics } from 'asyncActions/pageMetrics';
 
 export function useCalcPageMetrics() {
   const blocksRecord = useRecoilValue(previewBlocks);
@@ -9,17 +9,10 @@ export function useCalcPageMetrics() {
   const pageWidth = useRecoilValue(previewPageWidth);
   const setPreviewPages = useSetRecoilState(previewPages);
   const blocksLength = blocks.reduce((acc, v) => acc + v, 0);
-  const requester = useProcessMessage<
-    {
-      pageWidth: number;
-      blocks: number[];
-    },
-    Uint16Array
-  >();
 
   useEffect(() => {
     (async () => {
-      const metrics = await requester({ pageWidth, blocks });
+      const metrics = await calcuratePageMetrics({ pageWidth, blocks });
       const pages = splitEach(Array.from(metrics), 3).map(
         ([start, end, offset]: any) => ({
           offset,
@@ -31,31 +24,6 @@ export function useCalcPageMetrics() {
       setPreviewPages(pages);
     })();
   }, [blocksLength, pageWidth]);
-}
-
-function useProcessMessage<T, R>(): (message: T) => Promise<R> {
-  return useCallback(async (message: T) => {
-    const { done, value } = await workerGen().next();
-    if (done) {
-      throw new Error();
-    }
-
-    return await value.postMessage(message);
-  }, []);
-}
-
-async function* workerGen(): AsyncGenerator<
-  PromiseWorker,
-  PromiseWorker,
-  PromiseWorker
-> {
-  const workerResource = await fetch('workers://workers/pageMetrics/index.js');
-  const worker = new Worker(URL.createObjectURL(await workerResource.blob()));
-  const promised = new PromiseWorker(worker);
-
-  while (true) {
-    yield promised;
-  }
 }
 
 function splitEach<T>(arr: Array<T>, count: number): Array<T[]> {
